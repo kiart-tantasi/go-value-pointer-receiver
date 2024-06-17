8f0eae7639d34fc0f35d9bd46cd2e462da123259
kiart-tantasi <kiarttantasi@gmail.com>
2024-06-17 09:07:04 +0700

    also add into goroutine dir

--- goroutine/main.go
+++ goroutine/main.go
@@ -14,9 +14,11 @@ var mutex sync.Mutex
 func main() {
 	testValueGoroutine()
 	testPointerGoroutine()
+	testValueString()
+	testPointerString()
 }
 
-// === functions to test ==== //
+// === functions to test int ==== //
 func testValueGoroutine() {
 	var sum = 0
 	var wg sync.WaitGroup
@@ -35,7 +37,7 @@ func testValueGoroutine() {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Println("\n...value (goroutine) taken", time.Since(start))
+	fmt.Println("\n...int value taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
 func testPointerGoroutine() {
@@ -56,7 +58,54 @@ func testPointerGoroutine() {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Println("\n...pointer (goroutine) taken", time.Since(start))
+	fmt.Println("\n...int pointer taken", time.Since(start))
+	fmt.Println("...sum:", sum)
+}
+
+// === functions to test string ==== //
+const someString = "hello world"
+
+func testValueString() {
+	var sum = 0
+	var wg sync.WaitGroup
+	start := time.Now()
+	for i := 0; i < goroutines; i++ {
+		wg.Add(1)
+		go func(tempString string) {
+			tempSum := 0
+			for j := 0; j < roundsPerGoroutine; j++ {
+				tempSum += computeValueString(tempString)
+			}
+			mutex.Lock()
+			sum += tempSum
+			mutex.Unlock()
+			wg.Done()
+		}(someString)
+	}
+	wg.Wait()
+	fmt.Println("\n...string value taken", time.Since(start))
+	fmt.Println("...sum:", sum)
+}
+
+func testPointerString() {
+	var sum = 0
+	var wg sync.WaitGroup
+	start := time.Now()
+	for i := 0; i < goroutines; i++ {
+		wg.Add(1)
+		go func(tempString string) {
+			tempSum := 0
+			for j := 0; j < roundsPerGoroutine; j++ {
+				tempSum += computePointerString(&tempString)
+			}
+			mutex.Lock()
+			sum += tempSum
+			mutex.Unlock()
+			wg.Done()
+		}(someString)
+	}
+	wg.Wait()
+	fmt.Println("\n...string pointer taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
 
@@ -66,4 +115,10 @@ func computeValue(i int, j int) int {
 }
 func computePointer(i *int, j *int) int {
 	return *i * *j
+}
+func computeValueString(str string) int {
+	return len(str)
+}
+func computePointerString(str *string) int {
+	return len(*str)
 }
