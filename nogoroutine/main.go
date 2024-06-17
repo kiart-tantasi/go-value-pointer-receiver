478c1a1dfca718c5f61ea2400f11d0e0ebf62d4f
kiart-tantasi <kiarttantasi@gmail.com>
2024-06-17 09:01:28 +0700

    add string version

--- nogoroutine/main.go
+++ nogoroutine/main.go
@@ -12,11 +12,15 @@ const subRounds = 10_000_000
 var mutex sync.Mutex
 
 func main() {
+	// int
 	testValue()
 	testPointer()
+	// string
+	testValueString()
+	testPointerString()
 }
 
-// === functions to test ==== //
+// === functions to test int ==== //
 func testValue() {
 	var sum = 0
 	start := time.Now()
@@ -25,9 +29,10 @@ func testValue() {
 			sum += computeValue(i, j)
 		}
 	}
-	fmt.Println("\n...value taken", time.Since(start))
+	fmt.Println("\n...int value taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
+
 func testPointer() {
 	var sum = 0
 	start := time.Now()
@@ -36,7 +41,36 @@ func testPointer() {
 			sum += computePointer(&i, &j)
 		}
 	}
-	fmt.Println("\n...pointer taken", time.Since(start))
+	fmt.Println("\n...int pointer taken", time.Since(start))
+	fmt.Println("...sum:", sum)
+}
+
+// === functions to test string ==== //
+const someString = "hello world"
+
+func testValueString() {
+	var sum = 0
+	start := time.Now()
+	for i := 0; i < rounds; i++ {
+		for j := 0; j < subRounds; j++ {
+			tempString := someString
+			sum += computeValueString(tempString)
+		}
+	}
+	fmt.Println("\n...string value taken", time.Since(start))
+	fmt.Println("...sum:", sum)
+}
+
+func testPointerString() {
+	var sum = 0
+	start := time.Now()
+	for i := 0; i < rounds; i++ {
+		for j := 0; j < subRounds; j++ {
+			tempString := someString
+			sum += computePointerString(&tempString)
+		}
+	}
+	fmt.Println("\n...string pointer taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
 
@@ -46,4 +80,10 @@ func computeValue(i int, j int) int {
 }
 func computePointer(i *int, j *int) int {
 	return *i * *j
+}
+func computeValueString(str string) int {
+	return len(str)
+}
+func computePointerString(str *string) int {
+	return len(*str)
 }
