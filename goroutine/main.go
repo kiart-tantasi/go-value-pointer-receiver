75744d36fe63481f8034e4de12cfb7239e404a76
kiart-tantasi <kiarttantasi@gmail.com>
2024-06-18 20:36:32 +0700

    stop using sync

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 const goroutines = 10
-const roundsPerGoroutine = 10_000_000
-
-var mutex sync.Mutex
+const roundsPerGoroutine = 10_000_000_000
 
 func main() {
 	testValueGoroutine()
@@ -21,43 +18,39 @@ func main() {
 // === functions to test int ==== //
 func testValueGoroutine() {
 	var sum = 0
-	var wg sync.WaitGroup
 	start := time.Now()
+	values := make(chan int, goroutines)
 	for i := 0; i < goroutines; i++ {
-		wg.Add(1)
 		go func(i int) {
 			tempSum := 0
 			for j := 0; j < roundsPerGoroutine; j++ {
 				tempSum += computeValue(i, j)
 			}
-			mutex.Lock()
-			sum += tempSum
-			mutex.Unlock()
-			wg.Done()
+			values <- tempSum
 		}(i)
 	}
-	wg.Wait()
+	for range goroutines {
+		sum += <-values
+	}
 	fmt.Println("\n...int value taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
 func testPointerGoroutine() {
 	var sum = 0
-	var wg sync.WaitGroup
 	start := time.Now()
+	values := make(chan int, goroutines)
 	for i := 0; i < goroutines; i++ {
-		wg.Add(1)
 		go func(i int) {
 			tempSum := 0
 			for j := 0; j < roundsPerGoroutine; j++ {
 				tempSum += computePointer(&i, &j)
 			}
-			mutex.Lock()
-			sum += tempSum
-			mutex.Unlock()
-			wg.Done()
+			values <- tempSum
 		}(i)
 	}
-	wg.Wait()
+	for range goroutines {
+		sum += <-values
+	}
 	fmt.Println("\n...int pointer taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
@@ -67,44 +60,40 @@ const someString = "hello world"
 
 func testValueString() {
 	var sum = 0
-	var wg sync.WaitGroup
 	start := time.Now()
+	values := make(chan int, goroutines)
 	for i := 0; i < goroutines; i++ {
-		wg.Add(1)
 		go func(tempString string) {
 			tempSum := 0
 			for j := 0; j < roundsPerGoroutine; j++ {
 				tempSum += computeValueString(tempString)
 			}
-			mutex.Lock()
-			sum += tempSum
-			mutex.Unlock()
-			wg.Done()
+			values <- tempSum
 		}(someString)
 	}
-	wg.Wait()
+	for range goroutines {
+		sum += <-values
+	}
 	fmt.Println("\n...string value taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
 
 func testPointerString() {
 	var sum = 0
-	var wg sync.WaitGroup
 	start := time.Now()
+	values := make(chan int, goroutines)
 	for i := 0; i < goroutines; i++ {
-		wg.Add(1)
 		go func(tempString string) {
 			tempSum := 0
 			for j := 0; j < roundsPerGoroutine; j++ {
 				tempSum += computePointerString(&tempString)
 			}
-			mutex.Lock()
-			sum += tempSum
-			mutex.Unlock()
-			wg.Done()
+			values <- tempSum
 		}(someString)
 	}
-	wg.Wait()
+	for range goroutines {
+		sum += <-values
+	}
 	fmt.Println("\n...string pointer taken", time.Since(start))
 	fmt.Println("...sum:", sum)
 }
